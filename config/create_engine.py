eeeaf36dcfe0039ac4d9516293f802f691e3d714
Muhammad Hanzala Irfan <156577133+hanzalairfan@users.noreply.github.com>
2026-05-13 12:09:21 +0000

    Updated database connection code

--- config/create_engine.py
+++ config/create_engine.py
@@ -8,7 +8,11 @@ DB_PASSWORD= ''
 DB_HOST= 'localhost'
 DB_NAME= 'FYP'
 
+print("Database/SQL Credentials loaded successfully.")
+
 PASSWORD=quote_plus(DB_PASSWORD)
+
+print('Password successfully encoded.')
 
 try:
     ENGINE=create_engine(
@@ -16,12 +20,17 @@ try:
     )
 
     if ENGINE.connect:
+        print('Database connected successfully.')
         
         logging.info("Connection Built Successfully..........")
     
     else:
+        print('Connection Failed.')
+
         logging.error("Something Went Wrong WHILE **CONNECTING** .Try again")
 
 except SQLAlchemyError as e:
+    
+    print('SQLAlchemy error occured.')
     
     logging.error(e)
