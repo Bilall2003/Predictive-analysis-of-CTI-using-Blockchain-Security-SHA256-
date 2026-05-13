20b0a1f7eff3d36ed966f0679c39c215ed7bffe6
Muhammad Hanzala Irfan <156577133+hanzalairfan@users.noreply.github.com>
2026-05-13 12:40:46 +0000

    create engine modified

--- config/create_engine.py
+++ config/create_engine.py
@@ -1,36 +1,37 @@
+#importing libraries
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from urllib.parse import quote_plus
 import logging
 
+#loading credentials
 DB_USERNAME= 'root'
 DB_PASSWORD= ''
 DB_HOST= 'localhost'
 DB_NAME= 'FYP'
-
-print("Database/SQL Credentials loaded successfully.")
 
 PASSWORD=quote_plus(DB_PASSWORD)
 
 print('Password successfully encoded.')
 
 try:
+    # create_engine = an sqlalchemy function that creates a database connection setting up instructions and configuration to connect to mysql
+    # mysql+pymysql:// = tells sqlalchemy to use mysql and use pymysql driver to connect
     ENGINE=create_engine(
     f"mysql+pymysql://{DB_USERNAME}:{PASSWORD}@{DB_HOST}/{DB_NAME}"
     )
 
+    # checks if the connection is a success or not
     if ENGINE.connect:
-        print('Database connected successfully.')
         
         logging.info("Connection Built Successfully..........")
     
     else:
-        print('Connection Failed.')
 
         logging.error("Something Went Wrong WHILE **CONNECTING** .Try again")
-
+# Any sqlalchemy related errors be catched here
 except SQLAlchemyError as e:
     
     print('SQLAlchemy error occured.')
-    
+    # shows the exact error
     logging.error(e)
