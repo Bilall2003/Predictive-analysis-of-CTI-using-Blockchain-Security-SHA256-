0cf569cf464a6eb59a8521d7e60105962b4d7f6e
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-05-19 11:54:14 +0000

    dlt i.py

--- assets/i.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-list = ['assets/Monday-WorkingHours.csv','assets/Tuesday-WorkingHours.csv','assets/Wednesday-WorkingHours.csv','assets/Thursday-WorkingHours.csv']
-for i in list:
-    df = pd.read_csv(i)
-    print(i,df.shape)
