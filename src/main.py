483300094667611ffbc76e4f61906d679bc15a28
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-05-20 12:37:26 +0000

    update UI of dashboard

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from login import checkin
-from app import APP
+from app import NetworkIDSConsole
 import streamlit as st
 
 # Create the login object
@@ -8,8 +8,8 @@ obj1 = checkin()
 # This will show the login screen if not authorized
 if obj1.check_password():
     # Only if authorized is True, show the dashboard
-    obj2 = APP()
-    obj2.prototype()
+    obj2 = NetworkIDSConsole()
+    obj2.build_console_ui()
 else:
     # We stay on the login page
     st.info("Please log in to access the NIDS Intelligence Dashboard.")
