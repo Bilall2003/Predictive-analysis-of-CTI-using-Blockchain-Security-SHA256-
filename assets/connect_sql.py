7eeec687c655379c897397b9b898b059084562b3
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-08-31 18:58:49 +0500

    Refactor data loading and shuffling process
    Refactored data loading process to combine and shuffle datasets before insertion into the database.

--- assets/connect_sql.py
+++ assets/connect_sql.py
@@ -1,49 +1,4 @@
-# import numpy as np
-# import pandas as pd
-# from create_engine import ENGINE
-# from sqlalchemy import text
 
-# file_list = [
-#     r'C:\Users\UMAR.TECH\Desktop\cyber\3rd milestone\dataset_again\Tuesday-WorkingHours_reduced.csv',
-#     r'C:\Users\UMAR.TECH\Desktop\cyber\3rd milestone\dataset_again\Final_again_Wednesday.csv',
-#     r'C:\Users\UMAR.TECH\Desktop\cyber\3rd milestone\dataset_again\Final_again_Thursday.csv',
-#     r'C:\Users\UMAR.TECH\Desktop\cyber\3rd milestone\dataset_again\Final_again_Friday.csv',
-# ]
-
-# with ENGINE.begin() as conn:
-#     conn.execute(text("DROP TABLE IF EXISTS raw_sentiment_data"))
-
-# print("✅ Old table dropped. Starting fresh...\n")
-
-# # 🔥 STEP 2: LOAD DATA IN CHUNKS
-# for file in file_list:
-#     print(f"Processing: {file}")
-
-#     reader = pd.read_csv(file, chunksize=50000, low_memory=False)
-
-#     for chunk in reader:
-
-#         # Clean data
-#         chunk.replace([np.inf, -np.inf], np.nan, inplace=True)
-#         chunk.columns = [c.strip().lower() for c in chunk.columns]
-
-#         try:
-#             # Insert into MySQL
-#             chunk.to_sql(
-#                 name='IDS_DATA_2',
-#                 con=ENGINE,
-#                 if_exists='append',
-#                 index=False
-#             )
-
-#         except Exception as e:
-#             print(f"❌ Chunk failed: {e}")
-#             continue
-
-#     print(f"✅ Finished file: {file}")
-
-# print("\n🎯 ALL DATA SUCCESSFULLY LOADED")
-import numpy as np
 import pandas as pd
 from create_engine import ENGINE
 from sqlalchemy import text
